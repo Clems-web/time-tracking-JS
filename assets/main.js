dbb749d4328bac17a821f613f9ca1ffd14237bac
Clems-web <clement.servais1@gmail.com>
2022-01-21 13:19:21 +0100

    total time on project now appear when hovering project + get refresh everytime you enter/exit the div project

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,73 @@ let storage = window.localStorage;
 for (let projet of Object.keys(storage)) {
 
     projet = JSON.parse(storage[projet]);
-    new Project(projet.title, projet.clocks);
+    let essai = new Project(projet.title, projet.clocks);
+
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+    let tabHours = [];
+    let tabMinutes = [];
+    let tabSeconds = [];
+
+
+    for (let clock of projet.clocks) {
+        tabHours.push(clock.hours);
+        tabMinutes.push(clock.minutes);
+        tabSeconds.push(clock.seconds);
+    }
+
+    let hours = tabHours.reduce(reducer);
+    let minutes = tabMinutes.reduce(reducer);
+    let seconds = tabSeconds.reduce(reducer);
+
+
+    if (tabSeconds.reduce(reducer) > 59) {
+        seconds = seconds % 60;
+        minutes++;
+    }
+
+    if (tabMinutes.reduce(reducer) > 59) {
+        minutes = minutes % 60;
+        hours++;
+    }
+
+    essai.div.addEventListener('mouseenter', function () {
+
+        tabHours = [];
+        tabMinutes = [];
+        tabSeconds = [];
+
+
+        for (let clock of essai.clocks) {
+            tabHours.push(clock.hours);
+            tabMinutes.push(clock.minutes);
+            tabSeconds.push(clock.seconds);
+        }
+
+        let hours = tabHours.reduce(reducer);
+        let minutes = tabMinutes.reduce(reducer);
+        let seconds = tabSeconds.reduce(reducer);
+
+
+        if (tabSeconds.reduce(reducer) > 59) {
+            seconds = seconds % 60;
+            minutes++;
+        }
+
+        if (tabMinutes.reduce(reducer) > 59) {
+            minutes = minutes % 60;
+            hours++;
+        }
+        
+        this.querySelector('h2').innerHTML = hours + 'h ' + minutes + 'm ' + seconds + 's';
+
+    });
+
+    essai.div.addEventListener('mouseleave', function () {
+        this.querySelector('h2'). innerHTML = projet.title;
+    });
+
+
 }
 
 button.addEventListener('click', function () {
@@ -18,3 +84,4 @@ button.addEventListener('click', function () {
     projectContainer.append(new Project(inputTask.value).div);
     inputTask.value = '';
 })
+
